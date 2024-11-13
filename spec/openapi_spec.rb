d85ab7adebc11bd5ed2124dc85c7dc4ca88f4e37
Pablo Bello <pablobello@hey.com>
2024-11-13 21:12:11 +0000

    Add openapi_spec

---
+++ spec/openapi_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "OpenAPI document", type: :request do
+  subject(:schema) { skooma_openapi_schema }
+
+  it { is_expected.to be_valid_document }
+end
